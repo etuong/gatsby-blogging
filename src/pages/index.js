1729f18059b65b8b2d44471151137eb1c081b00e
etuong <etuong@gmail.com>
2022-07-24 19:36:41 -0700

    Port over new design

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,17 +8,113 @@ export default function Home({ data }) {
 
   return (
     <Layout>
-      <section>
-        <div>
-          <h2>Design</h2>
-          <h3>Develop & Deploy</h3>
-          <p>UX designer & web developer based in Manchester.</p>
-          <Link to="/projects">
-            My Portfolio Projects
-          </Link>
-        </div>
-        <GatsbyImage image={data.file.childImageSharp.gatsbyImageData} />
-      </section>
+	<div class="container">
+		<div class="mainheading">
+			<h1 class="sitetitle">Mediumish</h1>
+			<p class="lead">
+				Bootstrap theme, medium style, simply perfect for bloggers
+			</p>
+		</div>
+
+		<section class="featured-posts">
+			<div class="section-title">
+				<h2><span>Featured</span></h2>
+			</div>
+
+			<div class="card-columns listfeaturedtag">
+				<div class="card">
+					<div class="row">
+						<div class="col-md-5 wrapthumbnail">
+							<a href="post.html">
+								<div class="thumbnail" style="background-image:url(assets/img/demopic/1.jpg);">
+								</div>
+							</a>
+						</div>
+						<div class="col-md-7">
+							<div class="card-block">
+								<h2 class="card-title"><a href="post.html">We're living some strange times</a></h2>
+								<h4 class="card-text">This is a longer card with supporting text below as a natural
+									lead-in to additional content. This content is a little bit longer.</h4>
+								<div class="metafooter">
+									<div class="wrapfooter">
+										<span class="meta-footer-thumb">
+											<a href="author.html"><img class="author-thumb"
+													src="https://www.gravatar.com/avatar/e56154546cf4be74e393c62d1ae9f9d4?s=250&amp;d=mm&amp;r=x"
+													alt="Sal"></a>
+										</span>
+										<span class="author-meta">
+											<span class="post-name"><a href="author.html">Steve</a></span><br />
+											<span class="post-date">22 July 2017</span><span class="dot"></span><span
+												class="post-read">6 min read</span>
+										</span>
+										<span class="post-read-more"><a href="post.html" title="Read Story"><svg
+													class="svgIcon-use" width="25" height="25" viewbox="0 0 25 25">
+													<path
+														d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z"
+														fill-rule="evenodd"></path>
+												</svg></a></span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</section>
+
+		<section class="recent-posts">
+			<div class="section-title">
+				<h2><span>All Stories</span></h2>
+			</div>
+			<div class="card-columns listrecent">
+
+				<div class="card">
+					<a href="post.html">
+						<img class="img-fluid" src="assets/img/demopic/5.jpg" alt="">
+					</a>
+					<div class="card-block">
+						<h2 class="card-title"><a href="post.html">Autumn doesn't have to be nostalgic, you know?</a>
+						</h2>
+						<h4 class="card-text">This is a longer card with supporting text below as a natural lead-in to
+							additional content. This content is a little bit longer.</h4>
+						<div class="metafooter">
+							<div class="wrapfooter">
+								<span class="meta-footer-thumb">
+									<a href="author.html"><img class="author-thumb"
+											src="https://www.gravatar.com/avatar/e56154546cf4be74e393c62d1ae9f9d4?s=250&amp;d=mm&amp;r=x"
+											alt="Sal"></a>
+								</span>
+								<span class="author-meta">
+									<span class="post-name"><a href="author.html">Sal</a></span><br />
+									<span class="post-date">22 July 2017</span><span class="dot"></span><span
+										class="post-read">6 min read</span>
+								</span>
+								<span class="post-read-more"><a href="post.html" title="Read Story"><svg
+											class="svgIcon-use" width="25" height="25" viewbox="0 0 25 25">
+											<path
+												d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z"
+												fill-rule="evenodd"></path>
+										</svg></a></span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<div class="footer">
+			<p class="pull-left">
+				Copyright &copy; 2017 Your Website Name
+			</p>
+			<p class="pull-right">
+				Mediumish Theme by <a target="_blank" href="https://www.wowthemes.net">WowThemes.net</a>
+			</p>
+			<div class="clearfix">
+			</div>
+		</div>
+
+	</div>
     </Layout>
   );
 }
